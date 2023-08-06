c89dfd05242b8e05ee32aec6fdc349339f8f5206
amira <amira@amira-Lenovo-IdeaPad-Z510>
2023-08-06 19:08:53 +0300

    create delete file

---
+++ deleteDB.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+ read -p "please enter the database name " dbname
+ flag=0
+  while [ $flag -eq 0 ];do
+ 
+  if [[ -d ./databases/$dbname ]]
+  then
+  rm -r ./databases/$dbname
+  echo this database is deleted 
+  flag=1
+  elif [[ -z $dbname ]]
+  then
+  echo you must enter the database name
+  read -p "please enter the database name " dbname
+
+  else
+    echo this database name not exists
+    read -p "please enter the database name " dbname
+  fi
+  done
