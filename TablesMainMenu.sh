e7ddaf049abc0bb132c156036eec45d2051eb722
amira <amira@amira-Lenovo-IdeaPad-Z510>
2023-08-11 13:39:10 +0300

    edit file

--- TablesMainMenu.sh
+++ TablesMainMenu.sh
@@ -2,12 +2,12 @@
 
 echo "Welcome to Our Tables Main Menu in DataBase: "
 PS3='Please enter your choice: '
-options=("Create Table" "Show Table" "Drop Table" "Insert into Table" "Select from Table" "Delete from Table" "Update Table" "Quit")
+options=("Create Table" "Show Table" "Drop Table" "Insert into Table" "Select from Table" "Delete from Table" "Update Table" "Go back to database menu" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "Create Table")
-            bash ./CreateTable.sh
+            . ./createTable.sh
         ;;
         "List Table")
             bash ./ListTable.sh
@@ -16,10 +16,10 @@ do
             bash ./DropTable.sh
         ;;
         "Insert into Table")
-            bash ./InsertIntoTable.sh
+            bash ./insertTable.sh
         ;;
 	    "Select from Table")
-	        bash ./SelectFromTable.sh
+	        bash ./selectFromTable.sh
 	    ;;
         "Delete from Table")
 	        bash ./DeleteFromTable.sh
@@ -27,6 +27,9 @@ do
 	    "Update Table")
             bash ./UpdateTable.sh
 	    ;;
+        "Go back to database menu")
+        ./DBMainMenu.sh
+        ;;
         "Quit") exit
         ;;
         *) echo "Invalid option $REPLY"
