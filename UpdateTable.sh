f83c914435c93e4ce9d8a6a99c56bd87658b49f5
Toka <toka.farid.alhosarey@gmail.com>
2023-08-13 03:27:28 +0300

    final update

--- UpdateTable.sh
+++ UpdateTable.sh
@@ -2,12 +2,40 @@
 export LC_COLLATE=C             # Terminal Case Sensitive
 shopt -s extglob                #import Advanced Regex
 
+function checkColumnName {
+
+    local columnflag=0
+
+    while [ $columnflag -eq 0 ];do
+
+     if [[ $columnname == *['!''?'@\#\$%^\&*()-+\.\/';']* ]]
+    then
+    echo "column name can not contain any special character"
+    read -p "please enter a valid column name: " columnname
+    
+    elif [[ $columnname == *" "* ]] 
+    then
+    echo "column name can not contain spaces"
+    read -p "please enter a valid column name: " columnname
+    elif [[ $columnname =~ ^[0-9] ]]  
+    then
+    echo "column name can not begin with numbers"
+    read -p "please enter a valid column name: " columnname
+    
+    else
+    columnflag=1
+    fi
+done
+
+}
+
 echo "********************************"
 echo "..Here You Can Update Tables.."
 echo "********************************"
 echo "Tables in the $dbname database:"
-ls ./databases/$dbname/$tablename
+ls ./databases/$dbname/
 
+update_choice=0
 # Get the user's choice of whether to update the data or metadata or return back.
 while [[ $update_choice != "q" ]]; do
 # Ask the user if they want to update the table data or metadata
@@ -23,7 +51,7 @@ while true; do
 # Update table data
 # Ask the user which table they want to update
 echo "Which table do you want to update?"
-ls ./databases/$dbname/$tablename
+ls ./databases/$dbname/
 echo "============================"
 read tablename
 
@@ -91,102 +119,102 @@ if [[ $metadata_choice != "pk" && $metadata_choice != "dt" && $metadata_choice !
   continue
 fi
 
-# Change the PK
+# Check if the metadata choice is "pk"
 if [[ $metadata_choice == "pk" ]]; then
-  # Ask the user for the new PK name
-  echo "What is the new PK name?"
-  read new_pk_name
+    # Get the current PK name
+    current_pk_name=$(awk -F ':' '$3 == "yes" {print $1}' "./databases/$dbname/$tablename-metadata")
 
-  # Check if the new PK name is valid
-  if [[ ! $new_pk_name =~ ^[a-zA-Z]+$ ]]; then
-    echo "Invalid PK name. The name must be a string and must not have any spaces, special characters, or numbers."
-    continue
-  fi
+    # Display the current PK to the user
+    echo "The current primary key of this table is: $current_pk_name"
 
-  # Update the PK name in the metadata file
-  sed -i "1s/^pk:.*/pk:$new_pk_name/" "./databases/$dbname/$tablename-metadata"
-  echo "The PK has been successfully updated :)"
+    # Ask the user if they want to change the PK
+    echo "Do you want to change the primary key? (y/n)"
+    read change_pk_choice
+
+    # Check if the user wants to change the PK
+    if [[ $change_pk_choice == "y" ]]; then
+        # Display the column names to the user
+        column_names=$(cut -d ':' -f 1 "./databases/$dbname/$tablename-metadata")
+        echo "The available columns are: $column_names"
+
+        # Ask the user for the new PK column
+        echo "Please enter the name of the column you want to set as the new primary key:"
+        read new_pk_column
+
+        # Check if the new PK column is valid
+        if ! echo "$column_names" | grep -q -w "$new_pk_column"; then
+            echo "Invalid column name. Please enter a valid column name."
+            exit 1
+        fi
+
+        # Update the PK column in the metadata file
+        sed -i "s/^$current_pk_name:int:yes/$current_pk_name:int:no/" "./databases/$dbname/$tablename-metadata"
+        sed -i "s/^$new_pk_column:int:no/$new_pk_column:int:yes/" "./databases/$dbname/$tablename-metadata"
+        echo "The primary key has been successfully updated. The new primary key column is: $new_pk_column"
+    else
+        echo "No changes were made to the primary key."
+    fi
 fi
 
-# Change a datatype
+# Change the datatype
 if [[ $metadata_choice == "dt" ]]; then
-# Ask the user which column they want to change the datatype of
-echo "Which column do you want to change the datatype of? (specify by column number)"
+# Read the column names from the metadata file
+column_names=$(cut -d ':' -f 1 "./databases/$dbname/$tablename-metadata")
+
+# Display the available columns to the user
+echo "These are the columns of the table: "
+echo "$column_names"
+echo "Which column do you want to change the datatype of?"
 read col_choice
 
-# Check if the column number is valid
-if [[ $col_choice -lt 1 || $col_choice -gt $(awk -F: 'NR==2{print NF; exit}' ./databases/$dbname/$tablename-metadata) ]]; then
-  echo "The column number $col_choice is invalid."
-  exit 1
+# Check if the column name is valid
+if ! echo "$column_names" | grep -q -w "$col_choice"; then
+    echo "Invalid column name. Please enter a valid column name."
+    exit 1
 fi
 
 # Get the current datatype of the column
-current_datatype=$(head -n 2 ./databases/$dbname/$tablename-metadata | tail -n 1 | cut -d ':' -f $col_choice)
+current_datatype=$(grep -w "^$col_choice:" "./databases/$dbname/$tablename-metadata" | cut -d ':' -f 2)
 
 # Ask the user for the new datatype
-echo "What datatype do you want to change ($current_datatype) to?"
+# echo "The current datatype of column $col_choice is $current_datatype"
+echo "What datatype do you want to change it to? (Enter 'int' or 'str')"
 read new_datatype
 
 # Check if the input is valid
-while [[ $new_datatype != "int" && $new_datatype != "string" ]]; do
-  echo "Invalid datatype. Please enter either 'int' or 'string'."
-  read new_datatype
+while [[ $new_datatype != "int" && $new_datatype != "str" ]]; do
+    echo "Invalid datatype. Please enter either 'int' or 'str'."
+    read new_datatype
 done
 
 # Update the datatype in the metadata file
-sed -i "2s/[^:]*\(:\|$\)/$new_datatype\1/$col_choice" "./databases/$dbname/$tablename-metadata"
-echo "The datatype has been successfully updated :)"
+awk -v col="$col_choice" -v new_datatype="$new_datatype" 'BEGIN{FS=OFS=":"} $1 == col {$2 = new_datatype} 1' "./databases/$dbname/$tablename-metadata" > "./databases/$dbname/$tablename-metadata.tmp"
+mv "./databases/$dbname/$tablename-metadata.tmp" "./databases/$dbname/$tablename-metadata"
+
+echo "The datatype of column $col_choice has been successfully updated to $new_datatype."
 fi
 
 # Add a new column
 if [[ $metadata_choice == "col" ]]; then
 # Ask the user to enter the new column's data type
-echo "Please enter the new column's data type (int or string):"
+read -p "Please enter the new column's name: " newname
+checkColumnName "$columnname" 
+echo -n $newname":"  >>  ./databases/$dbname/$tablename-metadata
+
+echo "Please enter the new column's data type (int or str):"
 read new_data_type
 
 # Check that the new data type is either int or string
-if [[ $new_data_type != "int" && $new_data_type != "string" ]]; then
+if [[ $new_data_type != "int" && $new_data_type != "str" ]]; then
   echo "Sorry, The new data type must be either int or string."
   exit 1
 fi
 
 # Add the new data type to the end of the metadata file
-echo -n ":$new_data_type" >> ./databases/$dbname/$tablename-metadata
+echo -n $new_data_type":"  >> ./databases/$dbname/$tablename-metadata
+echo  "no"  >> ./databases/$dbname/$tablename-metadata
 
 # Inform the user that the tablename-metadata file has been updated successfully
-echo "The $tablename-metadata has been updated successfully."
-
-# Ask the user to enter the data for the new column
-echo "Please enter the data for the new column, column by column, based on the data type of each column: "
-
-# Read the data types from the tablename-metadata file
-data_types=$( cat ./databases/$dbname/$tablename-metadata )
-
-# Set the IFS variable to split the data types by colon
-IFS=":"
-
-# Loop through each data type and ask the user to enter the corresponding column's data
-for data_type in $data_types; do
-while true; do
-  echo "Please enter the data for the $data_type column:"
-  read column_data
-
-  # Check that the entered data matches the expected data type
-  if [[ $data_type == "int" && ! $column_data =~ ^-?[0-9]+$ ]]; then
-    echo "Sorry, The entered data for the $data_type column must be an integer, please enter again.."
-  elif [[ $data_type == "string" && ! $column_data =~ ^[a-zA-Z]+$ ]]; then
-    echo "Sorry, The entered data for the $data_type column must be a string, please enter again.."
-  else
-    # If the entered data is valid, break out of the while loop
-    break
-  fi
-  done
-
-  # Add the entered data to the end of the tablename file, using a colon separator
-  echo -n "$column_data:" >>  ./databases/$dbname/$tablename
-done
-
-# Inform the user that a new column has been created successfully in the tablename file
 echo "A new column has been created successfully in the $tablename."
 fi
 else
